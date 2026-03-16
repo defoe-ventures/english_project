a961b29754b227d01cc61541af350b4a738ab0a4
Brandon <brandon@defoeventures.com>
2026-03-16 15:16:37 +0200

    add student seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Student.create(name: "StudentA", level: "C+", invoicee: "yes", schedule: "2x weekly")!
